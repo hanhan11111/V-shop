a87639caec02a956f38249b1d5a59da8daff6ead
unknown <1815204223@qq.com>
2021-01-23 16:43:17 +0800

    add files

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Input, Form, FormItem, Button, Message } from 'element-ui'
+
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
